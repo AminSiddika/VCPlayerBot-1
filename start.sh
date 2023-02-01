daf416933599ce8502011012a2248b91591a88f2
AminSiddika <118524853+AminSiddika@users.noreply.github.com>
2023-02-01 16:38:27 +0600

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,10 +2,10 @@ echo "Cloning Repo...."
 if [ -z $BRANCH ]
 then
   echo "Cloning main branch...."
-  git clone https://github.com/AminSiddika/VCPlayerBot-1 /VCPlayerBot
+  git clone https://github.com/AminSiddika/VCPlayerBot-1 /VCPlayerBot-1
 else
   echo "Cloning $BRANCH branch...."
-  git clone https://github.com/AminSiddika/VCPlayerBot-1 -b $BRANCH /VCPlayerBot
+  git clone https://github.com/AminSiddika/VCPlayerBot-1 -b $BRANCH /VCPlayerBot-1
 fi
 cd /VCPlayerBot
 pip3 install -U -r requirements.txt
